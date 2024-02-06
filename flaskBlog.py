64a6a90704b7c0d03b036e1a8f454c3209fac692
ranjithreddy-31 <grreddy2726@gmail.com>
2024-02-05 22:42:56 -0600

    created the basic app

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,14 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Ranjith Reddy Gaddam',
+        'title': 'First Post',
+        'content': 'First Post content',
+        'date': '05 Feb, 2024'
+    },
+    {
+        'author': 'John Cena',
+        'title': 'Second Post',
+        'content': 'Second Post content',
+        'date': '05 Feb, 2024'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<H1>Home Page</H1>"
+    return render_template('home.html', posts = posts)
 
 @app.route("/about")
 def about():
-    return "<H1>About Page</H1>"
+    return render_template('about.html', title = "title")
 
 if __name__ == '__main__':
     app.run(port = 8000,debug = True)
