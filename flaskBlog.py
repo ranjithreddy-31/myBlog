eaa2b6a161d98e33a481cebe96adee8ebe63bfc2
ranjithreddy-31 <grreddy2726@gmail.com>
2024-02-06 22:03:58 -0600

    added registration and login forms

--- flaskBlog.py
+++ flaskBlog.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '6ce1b9ddb480ba0beb7a85f7ba4967d7'
 
 posts = [
     {
@@ -24,6 +27,25 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title = "title")
+
+@app.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account Created for {form.username.data}!!", 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title = "register", form = form)
+
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'grreddy2726@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Unsuccessful login. Please check your password', 'danger')
+    return render_template('login.html', title = "login", form = form)
 
 if __name__ == '__main__':
     app.run(port = 8000,debug = True)
